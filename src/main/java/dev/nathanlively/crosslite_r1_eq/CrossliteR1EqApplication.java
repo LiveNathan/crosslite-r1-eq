70e56d241de683e38c4c8ff5eb601e531a2d5324
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2025-09-12 13:58:32 -0500

    first

---
+++ src/main/java/dev/nathanlively/crosslite_r1_eq/CrossliteR1EqApplication.java
@@ -0,0 +1,13 @@
+package dev.nathanlively.crosslite_r1_eq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossliteR1EqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrossliteR1EqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/nathanlively/crosslite_r1_eq/CrossliteR1EqApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nathanlively.crosslite_r1_eq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrossliteR1EqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
